02be2a0d5d93e9612b01b93e65518210131f6cf3
Guo-lab <1811400940@qq.com>
2021-03-07 13:49:28 +0800

    remove std

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,3 +1,25 @@
+//! #意味着global
+//! 禁用std
+
+#![no_std]
+
+
+
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, Guo!");
 }
+
+
+
+
+use core::panic::PanicInfo;
+    // *************************************************************************************
+    // panic发生是调用(程序错误被迫停止或捕获机制处理)
+    // Diverging Function "never"type
+    // core donot need OS
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> !{
+    loop{}
+}
+    // *************************************************************************************
